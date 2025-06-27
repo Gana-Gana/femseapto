f53b8fa91aa0a59229ee73410fbc4540dc2ca486
nataliaguti22 <nggutierrez604@gmail.com>
2025-06-27 11:07:06 -0500

    Add allowance service component to home page

--- front/src/app/components/public/home/home.component.ts
+++ front/src/app/components/public/home/home.component.ts
@@ -4,11 +4,12 @@ import { Component } from '@angular/core';
 import { CarouselComponent } from '../home/components/carousel/carousel.component';
 import { CreditServiceHomeComponent } from '../home/components/credit-service-home/credit-service-home.component';
 import { SavingServiceHomeComponent } from '../home/components/saving-service-home/saving-service-home.component';
+import { AllowanceServiceComponent } from '../home/components/allowance-service/allowance-service.component'; 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselComponent, CreditServiceHomeComponent, SavingServiceHomeComponent],
+  imports: [CarouselComponent, CreditServiceHomeComponent, SavingServiceHomeComponent, AllowanceServiceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

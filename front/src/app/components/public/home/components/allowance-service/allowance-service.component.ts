08cc400d8986446ba983953aa9a32e40f2f67e3d
EliudQ <eliuddanielagq@gmail.com>
2025-07-10 17:34:20 -0500

    Update allowance service links to redirect to login

--- front/src/app/components/public/home/components/allowance-service/allowance-service.component.ts
+++ front/src/app/components/public/home/components/allowance-service/allowance-service.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-allowance-service',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './allowance-service.component.html',
   styleUrl: './allowance-service.component.css'
 })
